7d4ccf73f1218363556cd9608d1e8324ac444b14
irwanphan <irwanphan@gmail.com>
2023-06-22 12:00:45 +0700

    add map

--- libs/components/MapRenderer/index.tsx
+++ libs/components/MapRenderer/index.tsx
@@ -1,8 +1,13 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+
 const MapRenderer = () => {
     return (
-
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer 
+            style={{ height: '400px' }}
+            center={[51.505, -0.09]} 
+            zoom={13} 
+            scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,23 +1,33 @@
-import MapRenderer from "@components/MapRenderer"
-import BubbleContainer from "@elements/BubbleContainer"
-import IconicTitle from "@elements/IconicTitle"
-import PageSection from "@elements/Section"
-import MainLayout from "@layouts/MainLayout"
-import { FiMapPin } from "react-icons/fi"
+import { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import BubbleContainer from "@elements/BubbleContainer";
+import IconicTitle from "@elements/IconicTitle";
+import PageSection from "@elements/Section";
+import MainLayout from "@layouts/MainLayout";
+import { FiMapPin } from "react-icons/fi";
 
+// Dynamically import MapRenderer component to be used on the client-side
+const DynamicMapRenderer = dynamic(() => import("@components/MapRenderer"), { ssr: false });
 
 const PlaygroundPage = () => {
-    return (
-        <MainLayout>
-            <PageSection>
-                <BubbleContainer>
-                    <IconicTitle icon={FiMapPin} hoverColor='yellow.300'>Sight-C-Map</IconicTitle>
-                    <MapRenderer />
-                </BubbleContainer>
+  const [isClient, setIsClient] = useState(false);
 
-            </PageSection>
-        </MainLayout>
-    )
-}
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-export default PlaygroundPage
+  return (
+    <MainLayout>
+      <PageSection>
+        <BubbleContainer>
+          <IconicTitle icon={FiMapPin} hoverColor="yellow.300">
+            Sight-C-Map
+          </IconicTitle>
+          {isClient && <DynamicMapRenderer />}
+        </BubbleContainer>
+      </PageSection>
+    </MainLayout>
+  );
+};
+
+export default PlaygroundPage;
