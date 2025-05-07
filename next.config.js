cb9621b12e658fd9c6623245907cf4c02be20fe3
irwanphan <irwanphan@gmail.com>
2025-05-07 23:53:46 +0700

    update Next.js configuration to support PWA in development, set output options, and configure image optimization and base path for production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
 })
 
 module.exports = withPWA({
-  // next.js config
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/irwanphan.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/irwanphan.github.io/' : ''
 })
