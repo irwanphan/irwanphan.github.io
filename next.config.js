e0eb31fe82810db417866acfac407690c56b5f90
irwanphan <irwanphan@gmail.com>
2025-05-07 23:59:10 +0700

    refactor Next.js configuration by removing output options, image optimization settings, and base path, leaving a placeholder for additional configurations

--- next.config.js
+++ next.config.js
@@ -4,10 +4,5 @@ const withPWA = require('next-pwa')({
 })
 
 module.exports = withPWA({
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/irwanphan.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/irwanphan.github.io/' : ''
+  // next.js config lainnya
 })
