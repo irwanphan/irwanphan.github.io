e94b18507076ea4517d7929475c9c09c80b72281
irwanphan <irwanphan@gmail.com>
2025-05-08 00:20:59 +0700

    Refactor Next.js configuration by removing PWA support and cleaning up package dependencies in package.json and package-lock.json.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
-})
-
-module.exports = withPWA({
+const config = {
   output: 'export',
   distDir: 'out',
   images: {
     unoptimized: true
-  },
-})
+  }
+}
